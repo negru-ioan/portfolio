2bb9fbb6908440383735bdc33c4bcb2f35765662
negru-ioan <ioannegru808@gmail.com>
2023-06-08 14:45:53 +0300

    Updated the cv

--- data/global.ts
+++ data/global.ts
@@ -1,101 +1,101 @@
 type Route = {
-  title: string;
-  path: string;
+   title: string;
+   path: string;
 };
 
 type FooterCol = {
-  title: string;
-  links: {
-    name: string;
-    link: string;
-    icon?: string;
-    leavesWebsite: boolean;
-  }[];
+   title: string;
+   links: {
+      name: string;
+      link: string;
+      icon?: string;
+      leavesWebsite: boolean;
+   }[];
 };
 
 type Footer = {
-  columns: FooterCol[];
+   columns: FooterCol[];
 };
 
 export const routes: Route[] = [
-  {
-    title: "Acasă",
-    path: "/",
-  },
-  {
-    title: "Proiecte",
-    path: "/proiecte",
-  },
-  {
-    title: "Contacte",
-    path: "/contacte",
-  },
+   {
+      title: "Acasă",
+      path: "/",
+   },
+   {
+      title: "Proiecte",
+      path: "/proiecte",
+   },
+   {
+      title: "Contacte",
+      path: "/contacte",
+   },
 ];
 
 export const footer: Footer = {
-  columns: [
-    {
-      title: "Pagini",
-      links: [
-        {
-          name: "Acasă",
-          link: "/",
-          leavesWebsite: false,
-        },
-        {
-          name: "Proiecte",
-          link: "/proiecte",
-          leavesWebsite: false,
-        },
-        {
-          name: "Contacte",
-          link: "/contacte",
-          leavesWebsite: false,
-        },
-      ],
-    },
-    {
-      title: "Social",
-      links: [
-        {
-          name: "GitHub",
-          link: "https://github.com/negru-ioan",
-          icon: "./img/icons/github.svg",
-          leavesWebsite: true,
-        },
-        {
-          name: "LinkedIn",
-          link: "https://www.linkedin.com/in/ioan-negru-7b0730226/",
-          icon: "./img/icons/linkedin-f.svg",
-          leavesWebsite: true,
-        },
-        {
-          name: "Email",
-          link: "mailto:negruioan@duck.com",
-          icon: "./img/icons/mail-f.svg",
-          leavesWebsite: true,
-        },
-      ],
-    },
-    {
-      title: "Utile",
-      links: [
-        {
-          name: "CV",
-          link: "/utils/CV.pdf",
-          leavesWebsite: true,
-        },
-        {
-          name: "Resume",
-          link: "/utils/resume.pdf",
-          leavesWebsite: true,
-        },
-        {
-          name: "Apelează",
-          link: "tel:0742440724",
-          leavesWebsite: true,
-        },
-      ],
-    },
-  ],
+   columns: [
+      {
+         title: "Pagini",
+         links: [
+            {
+               name: "Acasă",
+               link: "/",
+               leavesWebsite: false,
+            },
+            {
+               name: "Proiecte",
+               link: "/proiecte",
+               leavesWebsite: false,
+            },
+            {
+               name: "Contacte",
+               link: "/contacte",
+               leavesWebsite: false,
+            },
+         ],
+      },
+      {
+         title: "Social",
+         links: [
+            {
+               name: "GitHub",
+               link: "https://github.com/negru-ioan",
+               icon: "./img/icons/github.svg",
+               leavesWebsite: true,
+            },
+            {
+               name: "LinkedIn",
+               link: "https://www.linkedin.com/in/ioan-negru-7b0730226/",
+               icon: "./img/icons/linkedin-f.svg",
+               leavesWebsite: true,
+            },
+            {
+               name: "Email",
+               link: "mailto:negruioan@duck.com",
+               icon: "./img/icons/mail-f.svg",
+               leavesWebsite: true,
+            },
+         ],
+      },
+      {
+         title: "Utile",
+         links: [
+            {
+               name: "CV",
+               link: "/utils/CV.pdf",
+               leavesWebsite: true,
+            },
+            {
+               name: "Resume",
+               link: "/utils/resume.pdf",
+               leavesWebsite: true,
+            },
+            {
+               name: "Apelează",
+               link: "tel:+37368611453",
+               leavesWebsite: true,
+            },
+         ],
+      },
+   ],
 };
