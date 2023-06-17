a83b8affcd404c2a6e216aacf1feed2faac2d72c
negru-ioan <ioannegru808@gmail.com>
2023-06-17 12:33:14 +0300

    Before Translating to English

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,11 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/",
+   plugins: [react()],
+   base: "/",
+   server: {
+      headers: {
+         "Cache-Control": "no-cache",
+      },
+   },
 });
