c7a47d8afe29d97d2a296ffd6742bece54a12e02
negru-ioan <ioannegru808@gmail.com>
2023-11-19 01:13:57 +0200

    fixed lint errors

--- src/components/contact/Popup.tsx
+++ src/components/contact/Popup.tsx
@@ -1,47 +1,51 @@
-import React, { useEffect } from "react";
-import ProgressBar from "./ProgressBar";
+import React, { useEffect, useCallback } from 'react';
+import ProgressBar from './ProgressBar';
 
 function Popup({ isOpen, closeModal, response }) {
-  useEffect(() => {
-    let timeoutId;
-    if (response) {
-      timeoutId = setTimeout(() => {
-        closeModal()
-        console.log(response)
-      }, 3400);
-    }
-    return () => clearTimeout(timeoutId);
-  }, [response]);
-  console.log(response);
-  return (
-    <div
-      className="modal-container w-full h-screen fixed top-0 left-0 z-[1000]"
-      onClick={closeModal}
-    >
-      <div className="modal-body fixed top-4 left-1/2 transform -translate-x-1/2 w-64 min-h-[16px] bg-white text-black rounded-2xl flex flex-col justify-center items-center text-5xl p-5 border-2 border-bg">
-        {isOpen && (
-          <div className="modal-overlay">
-            <div className="modal-content">
-              {response ? (
-                <>
-                  <h1 style={{ fontSize: 20 }}>{response}</h1>
-                  <ProgressBar />
-                </>
-              ) : (
-                <>
-                  <div className="modal-skeleton relative overflow-hidden w-5em min-h-3em bg-gradient-to-b from-gray-200 via-transparent to-gray-200 bg-repeat-y bg-size-150px-280px bg-position-0-5px bg-position-0-40px bg-position-0-75px bg-position-90px-75px bg-position-0-110px">
-                    <div className="skeleton-indicator absolute w-full h-full animate-skeleton bg-gray-300">
-                      <div className="shadow absolute w-0 bg-white" />{" "}
+    const memoizedCloseModal = useCallback(closeModal, [closeModal]);
+
+    useEffect(() => {
+        let timeoutId;
+        if (response) {
+            timeoutId = setTimeout(() => {
+                memoizedCloseModal();
+                console.log(response);
+            }, 3400);
+        }
+        return () => clearTimeout(timeoutId);
+    }, [response, memoizedCloseModal]);
+
+    console.log(response);
+
+    return (
+        <div
+            className="modal-container w-full h-screen fixed top-0 left-0 z-[1000]"
+            onClick={memoizedCloseModal}
+        >
+            <div className="modal-body fixed top-4 left-1/2 transform -translate-x-1/2 w-64 min-h-[16px] bg-white text-black rounded-2xl flex flex-col justify-center items-center text-5xl p-5 border-2 border-bg">
+                {isOpen && (
+                    <div className="modal-overlay">
+                        <div className="modal-content">
+                            {response ? (
+                                <>
+                                    <h1 style={{ fontSize: 20 }}>{response}</h1>
+                                    <ProgressBar />
+                                </>
+                            ) : (
+                                <>
+                                    <div className="modal-skeleton relative overflow-hidden w-5em min-h-3em bg-gradient-to-b from-gray-200 via-transparent to-gray-200 bg-repeat-y bg-size-150px-280px bg-position-0-5px bg-position-0-40px bg-position-0-75px bg-position-90px-75px bg-position-0-110px">
+                                        <div className="skeleton-indicator absolute w-full h-full animate-skeleton bg-gray-300">
+                                            <div className="shadow absolute w-0 bg-white" />{' '}
+                                        </div>
+                                    </div>
+                                </>
+                            )}
+                        </div>
                     </div>
-                  </div>
-                </>
-              )}
+                )}
             </div>
-          </div>
-        )}
-      </div>
-    </div>
-  );
+        </div>
+    );
 }
 
 export default Popup;
--- src/components/home/Skils.tsx
+++ src/components/home/Skils.tsx
@@ -1,49 +1,51 @@
-import React, { useEffect } from "react";
-import { skills } from "../../../data/content/home";
+import React from 'react';
+import { skills } from '../../../data/content/home';
 
 function Skills() {
-   const mobile = document.body.clientWidth;
-   return (
-      <div className="flex flex-col md:flex-row justify-between relative">
-         <h2 className="relative text-3xl w-full text-center md:text-left font-bold md:max-w-lg mb-10 md:mr-10 md:mb-0 md:w-max mr-0 ">
-            Here are the programming languages
-            and tools that I am proficient in.
-            <img
-               className="sqD bottom-[-80px] left-[-50px] lg:bottom-[-50px] lg:left-[-35px] z-[-10]"
-               src="./img/doodles/skills/laptop.svg"
-            />
-            <img
-               className="sqD hidden md:block top-[140px] right-0 lg:top-[105px] opacity-50 rotate-45"
-               src="./img/doodles/hero/code.svg"
-            />
-            <img
-               className="sqD hidden md:block top-[200px] right-[50px] lg:top-[170px] lg:right-[160px]"
-               src="./img/doodles/skills/youtube.svg"
-            />
-            <img
-               className="sqD top-[-15px] right-[-15px]"
-               src="./img/doodles/skills/fillStar.svg"
-            />
-         </h2>
-         <div className="relative max-w-lg w-full mx-auto md:mx-none grid gap-x-8 gap-y-12 sm:gap-8 md:gap-12 grid-cols-4 sm:grid-cols-5 items-center place-content-center">
-            {skills.map((item, index) => {
-               return (
-                  <div
-                     title={item.title}
-                     key={index}
-                     className="w-16 mx-auto flex items-center flex-col justify-center"
-                     style={{ display: mobile > 600 && item.mobile && "none" }}
-                  >
-                     <img src={item.icon} style={item.style} />
-                     <p className="text-xs text-fun-gray font-bold mt-3 opacity-80">
-                        {item.title}
-                     </p>
-                  </div>
-               );
-            })}
-         </div>
-      </div>
-   );
+    const mobile = document.body.clientWidth;
+    return (
+        <div className="flex flex-col md:flex-row justify-between relative">
+            <h2 className="relative text-3xl w-full text-center md:text-left font-bold md:max-w-lg mb-10 md:mr-10 md:mb-0 md:w-max mr-0 ">
+                Here are the programming languages and tools that I am
+                proficient in.
+                <img
+                    className="sqD bottom-[-80px] left-[-50px] lg:bottom-[-50px] lg:left-[-35px] z-[-10]"
+                    src="./img/doodles/skills/laptop.svg"
+                />
+                <img
+                    className="sqD hidden md:block top-[140px] right-0 lg:top-[105px] opacity-50 rotate-45"
+                    src="./img/doodles/hero/code.svg"
+                />
+                <img
+                    className="sqD hidden md:block top-[200px] right-[50px] lg:top-[170px] lg:right-[160px]"
+                    src="./img/doodles/skills/youtube.svg"
+                />
+                <img
+                    className="sqD top-[-15px] right-[-15px]"
+                    src="./img/doodles/skills/fillStar.svg"
+                />
+            </h2>
+            <div className="relative max-w-lg w-full mx-auto md:mx-none grid gap-x-8 gap-y-12 sm:gap-8 md:gap-12 grid-cols-4 sm:grid-cols-5 items-center place-content-center">
+                {skills.map((item, index) => {
+                    return (
+                        <div
+                            title={item.title}
+                            key={index}
+                            className="w-16 mx-auto flex items-center flex-col justify-center"
+                            style={{
+                                display: mobile > 600 && item.mobile && 'none',
+                            }}
+                        >
+                            <img src={item.icon} style={item.style} />
+                            <p className="text-xs text-fun-gray font-bold mt-3 opacity-80">
+                                {item.title}
+                            </p>
+                        </div>
+                    );
+                })}
+            </div>
+        </div>
+    );
 }
 
 export default Skills;
